7086a00e92ab892d279d8913943e21db246c9f17
EC2 Default User <ec2-user@ip-172-31-5-65.ap-northeast-1.compute.internal>
2023-01-18 11:26:57 +0000

    課題3(作業中1/18-3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-     #flash[:notice] = "Book was successfully updated."
+      flash[:complete] = "Book was successfully updated."
+      flash[:notice] = "Book was successfully updated."
      redirect_to user_path(@user.id)
     else
       render :edit
